5fa1e870fd57d4aa707366bd6ad4f5a3425d9386
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-06-21 15:25:50 +0200

    Initial commit

---
+++ Csharp - small projects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp___small_projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
