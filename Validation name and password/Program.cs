1f333ec8ac6cdd467464236d969d3ef49ac3c7d5
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-06-21 16:19:42 +0200

    Validation name and password

---
+++ Validation name and password/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Validation_name_and_password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
