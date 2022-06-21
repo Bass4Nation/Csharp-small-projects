f99adb039594108318fcd2f612a3313c581453ac
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-06-21 16:15:56 +0200

    Random number generator

---
+++ Random number generator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Random_number_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string INPUT_FIRST,INPUT_SECOND;
+            int start, end, result;
+            bool finalValidation = false;
+            Console.WriteLine("-------- Get random number --------");
+            while (!finalValidation)
+            {
+                //Will recive a start number from the user
+                Console.WriteLine("Write a start number: ");
+                INPUT_FIRST = Console.ReadLine();
+                start = Int32.Parse(INPUT_FIRST);
+                //Will recive a end number from the user
+                Console.WriteLine("Write a end number: ");
+                INPUT_SECOND = Console.ReadLine();
+                end = Int32.Parse(INPUT_SECOND);
+
+                finalValidation = checkSize(start, end);
+
+                if (finalValidation)
+                {
+                    result = new Random().Next(start, end);
+                    Console.WriteLine("Random number recived is : " + result);
+                }
+
+            }
+
+
+        }
+
+        static bool checkSize(int start, int end)
+        {
+            if(start >= end)
+            {
+                Console.WriteLine("Start number must be larger than end number");
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Random number generator/RandomNumber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Random_number_generator
+{
+    class RandomNumber
+    {
+        
+    }
+}
