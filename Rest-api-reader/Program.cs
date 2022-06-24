58da3ee2f9e9a55569dbd07b13080bf63cb26a9b
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-06-24 21:36:05 +0200

    REST api reader to console

---
+++ Rest-api-reader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Rest_api_reader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("------ REST Api Reader Console ---------- (under construction)");
+            Console.WriteLine("For a pokemon API");
+            string pokemon = "ditto";
+            string URL = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+            using var client = new HttpClient();
+
+            var content = await client.GetStringAsync(URL);
+            fromJson
+            Console.WriteLine(content);
+
+            Console.WriteLine("----------- END Pokemon API -------------------------");
+
+        }
+    }
+}
