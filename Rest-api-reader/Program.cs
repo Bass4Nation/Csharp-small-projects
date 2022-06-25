9eb9ac5addbecbc8cb3d501125b86274560be346
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-06-25 14:32:08 +0200

    Reading Pokemon API data using a pokemon name

--- Rest-api-reader/Program.cs
+++ Rest-api-reader/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,20 +8,52 @@ namespace Rest_api_reader
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("------ REST Api Reader Console ---------- (under construction)");
             Console.WriteLine("For a pokemon API");
-            string pokemon = "ditto";
-            string URL = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
-            using var client = new HttpClient();
+            string ditto = "ditto";
+            List<Task<Pokemon>> list = new List<Task<Pokemon>>();
+            list.Add(GetAllDataAsync(ditto));
 
-            var content = await client.GetStringAsync(URL);
-            fromJson
-            Console.WriteLine(content);
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i].Result.Name.ToString();
+            }
+
 
             Console.WriteLine("----------- END Pokemon API -------------------------");
 
+        }
+
+        private static async Task<Pokemon> GetAllDataAsync(string pokemon)
+        {
+            Pokemon pokemonData = new Pokemon();
+            try
+            {
+                string URL = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage httpResponse = await client.GetAsync(URL);
+                    if (httpResponse.IsSuccessStatusCode)
+                    {
+                        Debug.WriteLine("Status: " + httpResponse.StatusCode);
+                        var content = await client.GetStringAsync(URL);
+
+                        pokemonData = Pokemon.FromJson(content);
+                        Console.WriteLine(pokemonData.Name);
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Status Error Code: " + httpResponse.StatusCode);
+                    }
+                }
+            } catch( Exception e)
+            {
+                Debug.WriteLine(e);
+                throw;
+            }
+            return pokemonData;
         }
     }
 }
